28bd1ef1be46b4342c794402f68d8f8c36eff5b8
Younghyun Lee <younghyunlee22@gmail.com>
2023-12-08 14:29:28 -0600

    blank menu bar

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ const mainEl = document.querySelector("main");
 //Use querySelector because there's no id and it is only one element.
 console.log(mainEl);
 mainEl.style.backgroundColor = "var(--main-bg)";
+mainEl.innerHTML = "<h1>Dom manipulation</h1>";
+mainEl.classList.add("flex-ctr");
+
+const topMenuEl = document.getElementById("top-menu");
+topMenuEl.style.height = "100%";
+topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
+topMenuEl.classList.add("flex-around");
