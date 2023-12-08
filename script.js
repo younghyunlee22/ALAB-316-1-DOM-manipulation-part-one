2428080876ceabf921c27d669ebbab6c28b35963
Younghyun Lee <younghyunlee22@gmail.com>
2023-12-08 14:11:08 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("hi");
+
+const mainEl = document.querySelector("main");
+//Use querySelector because there's no id and it is only one element.
+console.log(mainEl);
+mainEl.style.backgroundColor = "var(--main-bg)";
