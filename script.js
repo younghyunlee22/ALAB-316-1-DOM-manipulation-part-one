2e126acae490b3a41f66c3e8225008d7da43d11c
Younghyun Lee <younghyunlee22@gmail.com>
2023-12-08 16:18:35 -0600

    Part 4: Adding Menu Interaction

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const mainEl = document.querySelector("main");
 //Use querySelector because there's no id and it is only one element.
-console.log(mainEl);
 mainEl.style.backgroundColor = "var(--main-bg)";
 mainEl.innerHTML = "<h1>Dom manipulation</h1>";
 mainEl.classList.add("flex-ctr");
@@ -13,9 +12,32 @@ topMenuEl.classList.add("flex-around");
 // Menu data structure
 var menuLinks = [
   { text: "about", href: "/about" },
-  { text: "catalog", href: "/catalog" },
-  { text: "orders", href: "/orders" },
-  { text: "account", href: "/account" },
+  {
+    text: "catalog",
+    href: "#",
+    subLinks: [
+      { text: "all", href: "/catalog/all" },
+      { text: "top selling", href: "/catalog/top" },
+      { text: "search", href: "/catalog/search" },
+    ],
+  },
+  {
+    text: "orders",
+    href: "#",
+    subLinks: [
+      { text: "new", href: "/orders/new" },
+      { text: "pending", href: "/orders/pending" },
+      { text: "history", href: "/orders/history" },
+    ],
+  },
+  {
+    text: "account",
+    href: "#",
+    subLinks: [
+      { text: "profile", href: "/account/profile" },
+      { text: "sign out", href: "/account/signout" },
+    ],
+  },
 ];
 
 menuLinks.forEach((ele) => {
@@ -23,4 +45,30 @@ menuLinks.forEach((ele) => {
   menu.setAttribute("href", ele.href);
   menu.innerHTML = ele.text;
   topMenuEl.append(menu);
+});
+
+const subMenuEl = document.getElementById("sub-menu");
+subMenuEl.style.height = "100%";
+subMenuEl.style.backgroundColor = "var(--sub-menu-bg)";
+subMenuEl.classList.add("flex-around");
+subMenuEl.style.position = "absolute";
+subMenuEl.style.top = "0";
+
+let topMenuLinks = topMenuEl.querySelectorAll("a");
+topMenuEl.addEventListener("click", function (e) {
+  e.preventDefault();
+  console.log(e.target.textContent);
+  if (e.target.tagName !== "A") {
+    return console.log("Invalid element");
+  } else {
+    e.target.classList.toggle("active");
+    console.log("target", e.target);
+
+    for (const link of topMenuLinks) {
+      if (link !== e.target) {
+        link.classList.remove("active");
+        console.log("other menus", link);
+      }
+    }
+  }
 });
